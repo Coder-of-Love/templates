7ac5aaa69b49327649537128b3cb1f8d45a4ca4a
coder_of_love <j.m.n.90@bk.ru>
2020-07-12 18:38:35 +0500

    refactor: Move idea live templates into seperate branch

--- react-redux-app-template/frontend/apps/common/js/utils/redux.js
+++
@@ -1,5 +0,0 @@
-import { fromJS, List, Map } from 'immutable';
-
-export const isImmutable = value => Map.isMap(value) || List.isList(value);
-
-export const setStoreValue = (state, key, value) => state.set(key, isImmutable(value) ? value : fromJS(value));
